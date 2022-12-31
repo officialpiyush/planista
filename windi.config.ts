73f9431e0db2e57c122156590184677b6a5f9c69
Piyush <bhangalepiyush@gmail.com>
2022-12-31 20:53:36 +0530

    chore: update config to include jetbrains mono

--- windi.config.ts
+++ windi.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from "vite-plugin-windicss";
 export default defineConfig({
   theme: {
     extend: {
+      fontFamily: {
+        jetbrains: ["JetBrains Mono", "monospace"],
+      },
       colors: {
         pastel: {
           red: "#FFB5A7",
