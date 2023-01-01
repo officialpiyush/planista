c48c41e6bee116f801249c68a0b7ed449fff4bac
Piyush <bhangalepiyush@gmail.com>
2023-01-01 13:01:01 +0530

    feat: update AddInput component to take onAdd function in props

--- src/components/AddInput.tsx
+++ src/components/AddInput.tsx
@@ -1,7 +1,18 @@
 import { useState } from "react";
 
-export default function AddInput() {
+interface AddInputProps {
+  onAdd: (value: string) => void;
+}
+
+export default function AddInput({ onAdd }: AddInputProps) {
   const [value, setValue] = useState("");
+
+  const onAddClicked = () => {
+    if (!value) return;
+
+    onAdd(value);
+    setValue("");
+  };
 
   return (
     <div className="flex gap-4">
@@ -14,6 +25,7 @@ export default function AddInput() {
       <button
         disabled={!value}
         type="button"
+        onClick={onAddClicked}
         className="disabled:(cursor-not-allowed opacity-40) px-4 py-2 bg-pastel-peach hover:(bg-pastel-peach/60) border border-black"
       >
         Add
